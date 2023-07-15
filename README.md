57edbb2aa7b6c8b52f8ab719c11eb1dc9dd0d199
Ronnie Bunao <ronniebunao001@gmail.com>
2023-07-15 10:58:57 +0800

    Add UI and JS functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Tic-Tac-Toe
+# Tic-Tac-Toe 
+-is a simple and fast game, where you have an option to choose your piece, either circle or cross. And your goal is to line up 3 circle or cross in a row before your opponent does.
---
+++ script.js
@@ -0,0 +1,94 @@
+
+
+const Gameboard = (()=>{
+    let gameBoard = ["","","","","","","","",""];
+    const restart = document.querySelector('#restart');
+    const boardBoxes = document.querySelectorAll('.game-board div')
+    let currentPlayer = null;
+
+    const displayController = () =>{
+        currentPlayer = player1;
+        boardBoxes.forEach(function(boardBox, index){
+            boardBox.addEventListener('click', function(){
+                if(gameBoard[index] === ""){
+                    gameBoard[index] = currentPlayer.mark;
+                    boardBox.textContent = currentPlayer.mark;
+                    console.log(gameBoard);
+                    winCondition();
+                    switchPlayer();
+                }
+            })
+        })
+    }
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const restartGame = function(){
+        gameBoard = ["","","","","","","","",""];
+        boardBoxes.forEach(button =>{
+            button.textContent = ""
+        })
+    }
+
+    const winCondition = function(){
+        if (
+            (currentPlayer.mark === gameBoard[0] && currentPlayer.mark === gameBoard[1] && currentPlayer.mark === gameBoard[2]) ||
+            (currentPlayer.mark === gameBoard[3] && currentPlayer.mark === gameBoard[4] && currentPlayer.mark === gameBoard[5]) ||
+            (currentPlayer.mark === gameBoard[6] && currentPlayer.mark === gameBoard[7] && currentPlayer.mark === gameBoard[8]) ||
+            (currentPlayer.mark === gameBoard[1] && currentPlayer.mark === gameBoard[4] && currentPlayer.mark === gameBoard[7]) ||
+            (currentPlayer.mark === gameBoard[0] && currentPlayer.mark === gameBoard[3] && currentPlayer.mark === gameBoard[6]) ||
+            (currentPlayer.mark === gameBoard[2] && currentPlayer.mark === gameBoard[5] && currentPlayer.mark === gameBoard[8]) ||
+            (currentPlayer.mark === gameBoard[0] && currentPlayer.mark === gameBoard[4] && currentPlayer.mark === gameBoard[8]) ||
+            (currentPlayer.mark === gameBoard[2] && currentPlayer.mark === gameBoard[4] && currentPlayer.mark === gameBoard[6])
+          ) {
+            alert(currentPlayer.win());
+          }else if(currentPlayer.win() === 'true'){
+            restartGame()
+          } else if(gameBoard.every(position => position !== "")){
+            alert(currentPlayer.draw());
+            restartGame()
+          }
+        };
+    
+
+    restart.addEventListener('click', () =>{
+        restartGame()
+    })
+    return{
+        displayController,
+    }
+})();
+
+
+const playerProto = {
+    win(){
+        return `${this.name} wins the game`
+    },
+    draw(){
+        return 'Draw'
+    }
+};
+
+function createdPlayer(name,mark){
+    const player = Object.create(playerProto);
+    player.name = name;
+    player.mark = mark;
+    return player;
+}
+const player1 = createdPlayer('Player1', 'X');
+const player2 = createdPlayer('Player2', 'O');
+const logs = Gameboard.displayController();
+
+
+
+
+
+
+
+
+
+
+
+
